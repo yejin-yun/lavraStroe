eb3782b79df40a87f0bba9b05bf1ce3734d0c14f
yejin-yun <yunjin2768@naver.com>
2021-05-09 22:20:30 +0900

    default

---
+++ src/main/java/com/example/lavrastore/LavraStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.lavrastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavraStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavraStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lavrastore/LavraStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lavrastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavraStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
