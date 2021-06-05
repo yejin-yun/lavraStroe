0abe39498672cd51fd2b42dd9bc37d6865bf4c79
Minjoo-kang123 <69236634+Minjoo-kang123@users.noreply.github.com>
2021-06-05 21:35:51 +0900

    로그인 로그아웃
    로그인 로그아웃

--- src/main/java/com/example/lavrastore/controller/LogoutController.java
+++ src/main/java/com/example/lavrastore/controller/LogoutController.java
@@ -2,8 +2,10 @@ package com.example.lavrastore.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class LogoutController {
 	@RequestMapping("/shop/logout.do")
 	public String handleRequest(HttpSession session) throws Exception {
