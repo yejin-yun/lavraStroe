218672bc650bc3a62a6031aee3ae00ddaf188b41
You-jeong136 <smj06168@gmail.com>
2021-06-21 22:27:35 +0900

    제출용 어쩌구

--- src/main/java/com/example/lavrastore/LavraStoreApplication.java
+++ src/main/java/com/example/lavrastore/LavraStoreApplication.java
@@ -2,13 +2,21 @@ package com.example.lavrastore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ComponentScan(basePackages="com.example.lavrastore.*")
-public class LavraStoreApplication {
+public class LavraStoreApplication  extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(LavraStoreApplication.class);
+	}
+
 
 	public static void main(String[] args) {//
 		SpringApplication.run(LavraStoreApplication.class, args);
